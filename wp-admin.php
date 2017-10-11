0ec911273ced11d56559c2f54bf6a82d4e22de75
Larry W. Cashdollar <larry0@me.com>
2017-10-11 16:39:46 -0400

    Update wp-admin.php

--- wp-admin.php
+++ wp-admin.php
@@ -1,18 +1,17 @@
 <?php
+#Larry W. Cashdollar
+#twitter @_larry0
+#Just a quick wordpress login logger.
 
-$log_file = '/path/to/log/wp-login.log';
-$site_name = "yoursitename.here";
+$log_file = '/var/log/wp/wp-login.log';
+$site_name = "www.yoursite.com";
+$title = "Your Website Name";
 
-
-echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en-US\"> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> <title>Bank Of Kun &rsaquo; Log In</title> <link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=3.8.3' type='text/css' media='all' /> <link rel='stylesheet' id='dashicons-css'  href='http://$site_name/wp-includes/css/dashicons.min.css?ver=3.8.3' type='text/css' media='all' /> <link rel='stylesheet' id='wp-admin-css'  href='http://$site_name/wp-admin/css/wp-admin.min.css?ver=3.8.3' type='text/css' media='all' /> <link rel='stylesheet' id='buttons-css'  href='http://$site_name/wp-includes/css/buttons.min.css?ver=3.8.3' type='text/css' media='all' /> <link rel='stylesheet' id='colors-fresh-css'  href='http://$site_name/wp-admin/css/colors.min.css?ver=3.8.3' type='text/css' media='all' /> <script type='text/javascript' src='http://$site_name/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script> <script type='text/javascript' src='http://$site_name/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script> <meta name='robots' content='noindex,follow' /> </head> <body class=\"login login-action-login wp-core-ui\"> <div id=\"login\"> <h1><a href=\"http://wordpress.org/\" title=\"Powered by WordPress\">Bank Of Kun</a></h1> <form name=\"loginform\" id=\"loginform\" action=\"http://$site_name/wp-login.php\" method=\"post\"> <p> <label for=\"user_login\">Username<br /> <input type=\"text\" name=\"log\" id=\"user_login\" class=\"input\" value=\"\" size=\"20\" /></label> </p> <p> <label for=\"user_pass\">Password<br /> <input type=\"password\" name=\"pwd\" id=\"user_pass\" class=\"input\" value=\"\" size=\"20\" /></label> </p> <p class=\"forgetmenot\"><label for=\"rememberme\"><input name=\"rememberme\" type=\"checkbox\" id=\"rememberme\" value=\"forever\"  /> Remember Me</label></p> <p class=\"submit\"> <input type=\"submit\" name=\"wp-submit\" id=\"wp-submit\" class=\"button button-primary button-large\" value=\"Log In\" /> <input type=\"hidden\" name=\"redirect_to\" value=\"http://$site_name/wp-admin/\" /> <input type=\"hidden\" name=\"testcookie\" value=\"1\" /> </p> </form> <p id=\"nav\"> <a href=\"http://$site_name/wp-login.php?action=lostpassword\" title=\"Password Lost and Found\">Lost your password?</a> </p> <script type=\"text/javascript\"> function wp_attempt_focus(){ setTimeout( function(){ try{ d = document.getElementById('user_login'); d.focus(); d.select(); } catch(e){} }, 200); } wp_attempt_focus(); if(typeof wpOnload=='function')wpOnload(); </script> <p id=\"backtoblog\"><a href=\"http://$site_name/\" title=\"Are you lost?\">&larr; Back to Bank Of Kun</a></p> </div> <link rel='stylesheet' id='ad-gallery-style-css'  href='http://$site_name/wp-content/plugins/wp-e-commerce-dynamic-gallery/assets/js/mygallery/jquery.ad-gallery.css?ver=3.8.3' type='text/css' media='all' /> <link rel='stylesheet' id='woocommerce_fancybox_styles-css'  href='http://$site_name/wp-content/plugins/wp-e-commerce-dynamic-gallery/assets/js/fancybox/fancybox.css?ver=3.8.3' type='text/css' media='all' /> <script type='text/javascript' src='http://$site_name/wp-content/plugins/wp-e-commerce-dynamic-gallery/assets/js/mygallery/jquery.ad-gallery.js?ver=3.8.3'></script> <script type='text/javascript' src='http://$site_name/wp-content/plugins/wp-e-commerce-dynamic-gallery/assets/js/fancybox/fancybox.min.js?ver=3.8.3'></script> <div class=\"clear\"></div> </body> </html>";
-
-	
 $ip       = $_SERVER['REMOTE_ADDR'];
 $logdate  = date(DATE_RFC2822);
 $login    = $_POST['log'];
 $password = $_POST['pwd'];
 $redirect = $_POST['redirect_to'];
-
 $data     = "===================================[Event $logdate Notice for $ip]=======================================\n";
 $data .= "Login:" . $login . "\n";
 $wp_submit = $_POST['wp_submit'];
@@ -24,3 +23,75 @@ foreach (getallheaders() as $name => $value) {
 $data .= "===========================================[Event End]============================================\n";
 file_put_contents($log_file, $data, FILE_APPEND | LOCK_EX);
 ?>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<?php
+	echo "<title>$title &lsaquo; Log In</title>";
+?>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<?php
+echo "<link rel='stylesheet' href='http://$site_name/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />";
+?>
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+<?php
+echo "		<h1><a href=\"https://wordpress.org/\" title=\"Powered by WordPress\" tabindex=\"-1\">$title</a></h1>";
+?>
+	
+<form name="loginform" id="loginform" action="wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="admin" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+<?php
+echo "		<input type=\"hidden\" name=\"redirect_to\" value=\"http://$site_name/wp-admin/\" />";
+?>
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+<?php
+echo "	<a href=\"http://$site_name/wp-login.php?action=lostpassword\">Lost your password?</a>";
+?>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_pass');
+d.value = '';
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+<?php
+echo  "	<p id=\"backtoblog\"><a href=\"http://$site_name/\">&larr; Back to $title</a></p>";
+?>
+	</div>
+		<div class="clear"></div>
+	</body>
+	</html>
